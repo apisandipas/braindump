b5002897dff2ee6eee44bd2757ac02e2f23b116d
Bryan Paronto <bparonto@gmail.com>
2019-08-10 21:38:02 -0500

    swaps out apollo server for express apollo server

--- server/src/index.js
+++ server/src/index.js
@@ -1,21 +1,28 @@
 import '@babel/polyfill'
 import path from 'path'
-import { ApolloServer } from 'apollo-server'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
 import dotenv from 'dotenv'
+import cors from 'cors'
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas'
 
 dotenv.config()
-import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas'
 
 const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')))
 const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')))
-
 const port = process.env.PORT || 8080
+const app = express()
+
+app.use(cors('*'))
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  playground: process.env.NODE_ENV === 'development'
 })
 
-server.listen({ port }).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`) // eslint-disable-line no-console
+server.applyMiddleware({ app })
+
+app.listen({ port }, () => {
+  console.log(`🚀  Server ready at http://locahost:${port}${server.graphqlPath}`) // eslint-disable-line no-console
 })
