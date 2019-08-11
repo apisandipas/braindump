4e19a2ca2a2398377d4060af856adeff478e51a8
Bryan Paronto <bparonto@gmail.com>
2019-08-10 22:42:02 -0500

    adds note crud

--- server/src/resolvers/note.js
+++ server/src/resolvers/note.js
@@ -1,12 +1,46 @@
 export default {
   Note: {},
   Query: {
+    getNote: async (parent, { id }, { models }) => {
+      try {
+        const note = await models.Note.findById(id)
+        return note.toJSON()
+      } catch (err) {
+        return new Error(err.message)
+      }
+    },
     allNotes: async (parent, args, { models }) => {
       try {
         const notes = await models.Note.fetchAll()
         return notes.toJSON()
       } catch (err) {
         return new Error(err.message)
+      }
+    }
+  },
+  Mutation: {
+    createNote: async (parent, args, { models }) => {
+      try {
+        const note = await models.Note.create(args)
+        return note.json()
+      } catch (err) {
+        return new Error(err.message)
+      }
+    },
+    updateNote: async (parent, args, { models }) => {
+      try {
+        const note = models.Note.update(args)
+        return note.toJSON()
+      } catch (err) {
+        return new Error(err.message)
+      }
+    },
+    deleteNote: async (parent, args, { models }) => {
+      try {
+        await models.Note.destroy(args)
+        return true
+      } catch (err) {
+        return false
       }
     }
   }
