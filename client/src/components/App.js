d8cd2e8e7a8c3cba8a48b501fe902ff4c00bd6b1
Bryan Paronto <bparonto@gmail.com>
2020-03-01 20:47:54 -0600

    removed client folder

--- client/src/components/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const App = () => {
-  return <div>Noteapp Client</div>
-}
-
-export default App
--- server/src/database.js
+++ server/src/database.js
@@ -1,7 +1,7 @@
 import knexConfig from '../knexfile'
 import Knex from 'knex'
 import bookshelf from 'bookshelf'
-import makeModelBase, { pluggable as baseModelPlugin } from 'bookshelf-modelbase'
+import makeModelBase from 'bookshelf-modelbase'
 import securePassword from 'bookshelf-secure-password'
 
 const env = process.env.NODE_ENV || 'development' // eslint-disable-line no-undef
@@ -14,7 +14,6 @@ db.plugin('virtuals')
 db.plugin('visibility')
 db.plugin('pagination')
 db.plugin('bookshelf-camelcase')
-db.plugin(baseModelPlugin)
 db.plugin(securePassword)
 
 export const ModelBase = makeModelBase(db)
